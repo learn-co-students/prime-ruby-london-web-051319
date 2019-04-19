229773cbd12d849ec5194f3aeac8a2ea969e4645
github username <github email address>
2019-04-19 11:07:06 +0200

    prime numbers method working

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
+require 'pry'
+
 # Add  code here!
+def prime?(number)
+	if number < 2
+		return false
+	# 2 is the first prime number, integers < 2 return false
+	else
+		x = (2..number-1).to_a
+		# creates array [2,3,...,number -1]
+
+		x.all? do |i|
+			number % i !=0
+		end
+		# then checks all? for truth whether number % array[i] != 0, if number1 % number2 = 0, number1 is not prime
+	end
+end
