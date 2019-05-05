928881830733eaa98161526a6da42d81c085bad9
Catherine Maritz <catherinemaritz1@gmail.com>
2019-05-05 09:44:30 +0100

    prime number method

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1 
+    return false
+  end
+  (2..number).each do |i|
+    if number % i == 0 && i < number
+      return false
+    end
+  end
+  true
+end
+
