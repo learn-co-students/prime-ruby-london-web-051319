409976b9581a0b09fb2fdef5ef16d1681bd67e50
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-05 17:00:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,13 +1,16 @@
+require 'pry'
+
 def prime?(num)
   
-  #counter = 2
+  counter = 2
   i = 1
-  num_range = (2..num-1).to_a
+  num_range = (counter..num-1).to_a
   
   if num > 1 
-    num_range = (2..num-1).to_a
+    #num_range = (2..num-1).to_a
     num_range.none? do | i | 
       num % i == 0
+      #binding.pry
     end
   else 
     return false
