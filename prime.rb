be9ddc9e04b56a1a674f4e6d1dd2f4ccfa73f53a
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-05 16:37:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 def prime?(num)
   
-  counter = 2
+  #counter = 2
+  i = 1
+  num_range = (2..num-1).to_a
+  
   if num > 1 
-    num_range = (counter..num-1).to_a
+    num_range = (2..num-1).to_a
     num_range.none? do | i | 
       num % i == 0
     end
